b388de72b7b2b1c5ba61e145b3a2032cdfd7f5b6
Kipronoh <133237638+Gideon-kipronoh@users.noreply.github.com>
2023-07-22 18:53:42 +0300

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title> ||ONLINE EXAMINATION SYSTEM </title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+<?php
+session_start();
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+  //something was posted
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  if(!empty($email) && !empty($password) && !is_numeric($email))
+  {
+    $query = "select * from login where email = '$email' limit 1";
+    $result = mysqli_query($con, $query);
+    if($result)
+    {
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            $user_data = mysqli_fetch_assoc($result);
+            if($user_data['password'] == $password)
+            {
+                $_SESSION['email'] = $user_data['email'];
+                header("Location: index.php");
+                die;
+            }
+        }
+    }  
+    echo '<script>alert("wrong username or password")</script>';
+}
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ONLINE EXAMINATION</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="bg" style="background-image:url(image/bg.jpg);">
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logo"> ONLINE EXAMINATION SYSTEM</span></div>
+<div class="col-md-2 col-md-offset-4">
+<a href="#" class="pull-right btn sub1" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Signin</b></span></a></div>
+            </div>
+    
+<!--Footer start-->
+<div class="row footer">
+<div class="col-md-3 box">
+<a href="dash.php" target="_blank">Dashboard</a>
+</div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#login">Admin Login</a></div>
+<div class="col-md-3 box">
+<a href="#" data-toggle="modal" data-target="#developers">Developer</a>
+</div>
+<div class="col-md-3 box">
+<a href="feedback.php" target="_blank">Feedback</a></div></div>
+<!-- Modal For Developers-->
+<div class="modal fade title1" id="developers">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developer</span></h4>
+      </div>
+	  
+      <div class="modal-body">
+        <p>
+		<div class="row">
+		
+		 <div class="col-md-5">
+		<a href="http://yugeshverma.blogspot.in" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">kiprono gideon</a>
+		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+2541234567</h4>
+		<h4 style="font-family:'typo' ">kipronoyegon68@gmail.com</h4>
+		<h4 style="font-family:'typo' ">S.T Paul's University </h4></div></div>
+		</p>
+      </div>
+    
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<!--Modal for admin login-->
+	 <div class="modal fade" id="login">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">LOGIN</span></h4>
+      </div>
+      <div class="modal-body title1">
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-6">
+<form role="form" method="post" action="admin.php?q=index.php">
+<div class="form-group">
+<input type="text" name="uname" maxlength="20"  placeholder="Admin user id" class="form-control"/> 
+</div>
+<div class="form-group">
+<input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+</div>
+<div class="form-group" align="center">
+<input type="submit" name="login" value="Login" class="btn btn-primary" />
+</div>
+</form>
+</div><div class="col-md-3"></div></div>
+      </div>
+      <!--<div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>-->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--footer end-->
+<div class="content">
+
+            <h2 align="center" style="font-family:'typo'; color:#000066"> About us</h2>
+            <p style="font-family:'typo'; color:#000066" class="par">At kipronohGideon technology, we specialize in providing advanced and comprehensive online examination management solutions that empower students to streamline operations and enhance learning experiences. With our state-of-the-art technology and industry expertise, we are committed to helping students thrive in the dynamic fields.<br />This Platform was created by kipronoh Gideon</p>
+            </div>
+</body>
+</html>
